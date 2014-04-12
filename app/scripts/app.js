28ffe79f1f5a9a4f863cddc49d586748c88847d7
rj <rj@rj-ThinkPad-X61.(none)>
2014-04-12 14:43:32 -0400

    .

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular
+var app = angular
   .module('sendHappApp', [
     'ngCookies',
     'ngResource',
@@ -11,6 +11,30 @@ angular
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/completePurchase', {
+        templateUrl: 'views/completePurchase.html',
+        controller: 'MainCtrl'
+      })
+      .when('/detailProduct', {
+        templateUrl: 'views/detailProduct.html',
+        controller: 'MainCtrl'
+      })
+      .when('/friendsList', {
+        templateUrl: 'views/friendsList.html',
+        controller: 'MainCtrl'
+      })
+      .when('/purchase', {
+        templateUrl: 'views/purchase.html',
+        controller: 'MainCtrl'
+      })
+      .when('/setting', {
+        templateUrl: 'views/setting.html',
+        controller: 'MainCtrl'
+      })
+      .when('/shop', {
+        templateUrl: 'views/shop.html',
         controller: 'MainCtrl'
       })
       .otherwise({
