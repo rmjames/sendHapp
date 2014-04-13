a8d2f507248f6d4df1720b39f2ec41c12a3c4b87
rj <rj@rj-ThinkPad-X61.(none)>
2014-04-13 12:24:25 -0400

    changed routes/pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,35 +10,23 @@ var app = angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
+        templateUrl: 'views/login.html',
+        controller: 'MainCtrl'
+      })
+      .when('/connectSpotify', {
+        templateUrl: 'views/connectSpotify.html',
+        controller: 'mainCtrl'
+      })
+      .when('/finish', {
+        templateUrl: 'views/finish.html',
+        controller: 'MainCtrl'
+      })
+      .when('/main', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/completePurchase', {
-        templateUrl: 'views/completePurchase.html',
-        controller: 'mainCtrl'
-      })
-      .when('/detailProduct', {
-        templateUrl: 'views/detailProduct.html',
-        controller: 'MainCtrl'
-      })
-      .when('/friendsList', {
-        templateUrl: 'views/friendsList.html',
-        controller: 'MainCtrl'
-      })
-      .when('/purchase', {
-        templateUrl: 'views/purchase.html',
-        controller: 'MainCtrl'
-      })
-      .when('/setting', {
-        templateUrl: 'views/setting.html',
-        controller: 'MainCtrl'
-      })
-      .when('/shop', {
-        templateUrl: 'views/shop.html',
-        controller: 'MainCtrl'
-      })
-      .when('/person', {
-        templateUrl: 'views/person.html',
+      .when('/quantity', {
+        templateUrl: 'views/quantity.html',
         controller: 'MainCtrl'
       })
       .otherwise({
